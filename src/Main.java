1f0057147f6b146f33eea30504c4f807804a1345
mPeskov23 <sorrowdelivery@gmail.com>
2024-12-29 22:54:21 +0100

    First commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * @author Mikhail Peskov
+ * @version 0.1
+ */
+
+// Main class
+public class Main {
+    // Entry point
+    public static void main(String[] args) {
+
+        /*
+        The method prints a line to the console
+        */
+        System.out.println("Hello"); // shortcut "sout"
+
+    }
+}
